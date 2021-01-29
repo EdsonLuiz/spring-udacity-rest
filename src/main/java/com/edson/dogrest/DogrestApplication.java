80981c3828b9037564e77f91c785113ea88c084b
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-01-29 14:31:34 -0300

    chore: start project

---
+++ src/main/java/com/edson/dogrest/DogrestApplication.java
@@ -0,0 +1,13 @@
+package com.edson.dogrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edson/dogrest/DogrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edson.dogrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
