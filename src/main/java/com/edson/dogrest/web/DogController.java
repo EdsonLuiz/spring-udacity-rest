7770d95c5742d5941144a6c6993fd826763acb1d
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-01-29 18:54:25 -0300

    chore: add data.sql

--- src/main/java/com/edson/dogrest/web/DogController.java
+++ src/main/java/com/edson/dogrest/web/DogController.java
@@ -1,8 +1,18 @@
 package com.edson.dogrest.web;
 
+import com.edson.dogrest.entity.Dog;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DogController {
     
+    @GetMapping("/dogs")
+    public ResponseEntity<Dog> getDog() {
+        var dog = new Dog("Some Dog", "Some breed", "Some origin");
+        return new ResponseEntity<Dog>(dog, HttpStatus.OK);
+    }
 }
